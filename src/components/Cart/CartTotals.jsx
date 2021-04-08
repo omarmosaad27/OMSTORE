c8be275f899f7c9087113b29f3b89cbbff1f9fb8
omarmosaad27 <omarmosaada27@gmail.com>
2021-04-08 19:07:29 +0200

    update

--- src/components/Cart/CartTotals.jsx
+++ src/components/Cart/CartTotals.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-const CartTotals = ({value}) => {
+import PaypalButton from './PaypalButton'
+const CartTotals = ({value, history}) => {
     const {cartSubtotal, cartTax, cartTotal, clearCart} = value
     return (
         <>
@@ -38,6 +39,7 @@ const CartTotals = ({value}) => {
                                 ${cartTotal}
                             </strong>
                         </h5>
+                        <PaypalButton total={cartTotal} clearCart={clearCart} history={history}/>
                     </div>
                 </div>
             </div>
